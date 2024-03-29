b0a50bc6a9dc27d90b6268dc9fca2fb86d34549e
DILAWERRAHIM <dilaweralilinux@gmail.com>
2024-03-29 23:29:36 +0500

    ready to work for hire-me portion

--- frontend/components/header/Name.jsx
+++ frontend/components/header/Name.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import {Phone} from "../Icons/icons"
-import { Email } from '../Icons/icons'
+import React from "react";
+import { Phone } from "../Icons/icons";
+import { Email } from "../Icons/icons";
 const Name = () => {
   return (
-    <div className='m-4 text-left text-white'>
-       
-        <h1 className='text-2xl'> Dilawer Ali</h1>
+    <div className="m-4 text-left text-white">
+      <h1 className="text-2xl"> Dilawer Ali</h1>
 
-        <div className='flex text-sm'>
-            <Phone/>
-            <div className='text-sm mx-1' > 03090812627</div>
-        </div>
-        <div className='flex'>
-        <Email/>
-           <div className='text-sm mx-1'> aliwebdeveloper04@gmail.com</div>
-        </div>
-
-
+      <div className="flex text-sm">
+        <Phone />
+        <a href="https://web.whatsapp.com/" className="text-sm mx-1">
+          {" "}
+          +923090812627
+        </a>
+      </div>
+      <div className="flex">
+        <Email />
+        <a
+          href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+          className="text-sm mx-1"
+        >
+          {" "}
+          aliwebdeveloper04@gmail.com
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Name
+export default Name;
